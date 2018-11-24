3be64170cc8a4c854b4fc2c954a1bbd2d3ded8d5
Zhulkevskyi Vladyslav <zhulkevskijv@gmail.com>
2018-11-24 18:30:24 +0200

    Copy code from practice and import bootstrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+import './scss/main.scss';
+//import 'bootstrap';	// with JS!!
+import 'bootstrap/dist/css/bootstrap.min.css';	// only minified CSS
 console.log("Hello deadlines");
 console.log(`The time is ${new Date()}`);
-import './scss/main.scss';
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+let _makeProduct = require('./modules/product-html');
+
+jQuery.ajax({
+    url: 'https://nit.tron.net.ua/api/product/list',
+    method: 'get',
+    dataType: 'json',
+    success: function(json){
+        console.log('Loaded via AJAX!');
+        // console.log(json);
+        console.table(json);
+
+        json.forEach(product => $('.product-grid').append(_makeProduct(product)));
+        console.log('Added to grid');
+    },
+    error: function(xhr){
+        alert("An error occured: " + xhr.status + " " + xhr.statusText);
+    },
+});
