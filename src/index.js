432ea2d8ba728c8f83bc6badd33ecbfa4aa5bcf4
Zhulkevskyi Vladyslav <zhulkevskijv@gmail.com>
2018-11-30 01:58:29 +0200

    Empty cart option, dropdown item name changed when picking some category and some little improvements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './scss/main.scss';
-//import 'bootstrap/dist/css/bootstrap.min.css';	// only minified CSS
-console.log(`The time is ${new Date()}`);
 
 import $ from 'jquery';
-
 window.jQuery = $;
 window.$ = $;
 
@@ -13,13 +10,18 @@ import 'bootstrap/js/dist/modal';
 let _makeProduct = require('./modules/product-html');
 let _makeCategory = require('./modules/product-categories');
 let _makeModal = require('./modules/modal-description');
+let _arrayCartFunctions=require('./modules/cart');
+let _makeEmptyCart=_arrayCartFunctions[0];
+let productArray=[];
+
+//functions
+//adds all products to product grid
 jQuery.ajax({
     url: 'https://nit.tron.net.ua/api/product/list',
     method: 'get',
     dataType: 'json',
     success: function (json) {
         console.log('Loaded via AJAX!');
-        // console.log(json);
         console.table(json);
 
         json.forEach(product => $('.product-grid').append(_makeProduct(product)));
@@ -30,6 +32,7 @@ jQuery.ajax({
     },
 });
 
+//adds categories to button "categories"
 jQuery.ajax({
     url: 'https://nit.tron.net.ua/api/category/list',
     method: 'get',
@@ -46,8 +49,10 @@ jQuery.ajax({
     },
 });
 
+//adds products by clicking button "All products"
 $(document).on('click', '.button-all-products', function () {
     $(".product-grid").children("*").remove();
+    $(".dropdown-toggle").text("Categories");
     jQuery.ajax({
         url: 'https://nit.tron.net.ua/api/product/list',
         method: 'get',
@@ -66,9 +71,12 @@ $(document).on('click', '.button-all-products', function () {
     });
 });
 
+//adds products of chosen category
+//the category is chosen in dropdown menu "Categories"
 $(document).on('click', '.category', function () {
     let idCategory=$(this).attr("data-category-id");
     $(".product-grid").empty();
+    $(".dropdown-toggle").text($(this).text());
     jQuery.ajax({
         url: `https://nit.tron.net.ua/api/product/list/category/${idCategory}`,
         method: 'get',
@@ -84,11 +92,12 @@ $(document).on('click', '.category', function () {
         },
     });
 });
-
+// shows pop-up window when clicking on any information of the product
 $(document).on('click', '.description-toggle', function () {
     let idProduct=$(this).attr("data-product-id");
     $(".modal-body").empty();
     $(".modal-header").empty();
+    $(".modal-footer").empty();
     jQuery.ajax({
         url: `https://nit.tron.net.ua/api/product/${idProduct}`,
         method: 'get',
@@ -100,4 +109,19 @@ $(document).on('click', '.description-toggle', function () {
             alert("An error occured: " + xhr.status + " " + xhr.statusText);
         },
     });
+});
+
+$(document).on('click','.buy', function () {
+
+});
+// if the client didn't buy anything, the cart is empty
+// else make a menu to place an order
+$(document).on('click', '.cart', function () {
+    $(".modal-body").empty();
+    $(".modal-header").empty();
+    if (productArray.length===0){
+        _makeEmptyCart();
+    } else{
+
+    }
 });
